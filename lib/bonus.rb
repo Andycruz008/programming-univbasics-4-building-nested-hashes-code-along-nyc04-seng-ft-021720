07d5c4412794e477da05f5e16e2f7000c20b6ee7
concurrent-server-0630 <andycruz008@gmail.com>
2020-02-13 22:22:24 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,8 @@ def bonus
   }
 
   #code your solution here:
-hero
+hero[:status] =
+heroine[:status]
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
